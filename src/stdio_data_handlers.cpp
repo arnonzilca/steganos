d4035874d70dc35056b4a631d619fb56f9ff724b
Ohad Lutzky <ohad@lutzky.net>
2013-06-28 17:58:36 +0300

    Add rudimentary EOF handling
    Affects #3

--- src/stdio_data_handlers.cpp
+++ src/stdio_data_handlers.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "stdio_data_handlers.h"
 
@@ -9,13 +10,29 @@ void StdioCovertDataReceiver::loopDataHandling() const {
 }
 
 void StdioCovertDataReceiver::handleReceivedByte(unsigned char byte) {
-    fputc(byte, stdout);
+	// Note that the received byte is an actual byte, and cannot be EOF. So
+	// without an additional mechanism to signify EOF over the channel, we
+	// can't quit on EOF here.
+	fputc(byte, stdout);
+
+	// Since we have no mechanism of "proper closing", and output might be to a
+	// file, we have to make sure we flush output before we're killed.
+	fflush(stdout);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 void StdioCovertDataTransmitter::loopDataHandling() const {
 	while (true) {
-		covertDataTransmitter.transmitByte(getchar());
+		int charToSend = getchar();
+
+		// Upon EOF, getchar() will return with EOF ad infinitum, causing us to
+		// spam the receiving end. Instead, we quit on EOF, like ordinary
+		// data-driven unix programs.
+		if (charToSend == EOF) {
+			exit(0);
+		}
+
+		covertDataTransmitter.transmitByte(charToSend);
 	}
 }
